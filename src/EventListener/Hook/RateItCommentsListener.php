f3c931012f4fd056e1145c44e170e58728d037e4
David Molineus <david.molineus@netzmacht.de>
2020-01-31 08:43:03 +0100

    Generate more explicit rate it title

--- src/EventListener/Hook/RateItCommentsListener.php
+++ src/EventListener/Hook/RateItCommentsListener.php
@@ -16,11 +16,14 @@ declare(strict_types=1);
 
 namespace Hofff\Contao\RateIt\EventListener\Hook;
 
+use Contao\Controller;
 use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Contao\Model;
+use Contao\System;
 use Contao\Template;
 use Doctrine\DBAL\Connection;
 use Hofff\Contao\RateIt\Rating\RatingService;
+use PDO;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use function class_exists;
 use function strncmp;
@@ -78,7 +81,11 @@ final class RateItCommentsListener extends RatingListener
                 'tstamp'       => time(),
                 'typ'          => 'comments',
                 'createdat'    => time(),
-                'title'        => $GLOBALS['TL_LANG']['MSC']['com_by'] . ' ' . $template->name,
+                'title'        => $GLOBALS['TL_LANG']['MSC']['com_by']
+                    . ' '
+                    . $template->name
+                    . ' - '
+                    . $this->generateTitle($template->source, $template->parent),
                 'active'       => '1',
                 'parentstatus' => 'a',
             ]
@@ -117,5 +124,80 @@ final class RateItCommentsListener extends RatingListener
             $parentRecord->pid,
             false
         );
+    }
+
+    private function generateTitle(string $source, $sourceId) : string
+    {
+        Controller::loadLanguageFile('tl_comments');
+
+        $title  = $GLOBALS['TL_LANG']['tl_comments'][$source] ?? $source;
+        $title .= ' ' . $sourceId;
+
+        if ($source === 'tl_content') {
+            $statement = $this->connection->prepare('SELECT ptable,pid FROM tl_content WHERE id=:id LIMIT 0,1');
+            $statement->execute(['id' => $sourceId]);
+
+            if ($statement->rowCount() === 0) {
+                return $title;
+            }
+
+            $source   = $statement->fetchColumn(0) ?: 'tl_article';
+            $sourceId = $statement->fetchColumn(1);
+        }
+
+        switch ($source) {
+            case 'tl_page':
+                $statement = $this->connection->prepare('SELECT title FROM tl_page WHERE id=? LIMIT 0,1');
+
+                break;
+
+            case 'tl_news':
+                $statement = $this->connection->prepare('SELECT headline FROM tl_news WHERE id=?');
+
+                break;
+
+            case 'tl_faq':
+                $statement = $this->connection->prepare('SELECT question FROM tl_faq WHERE id=?');
+
+                break;
+
+            case 'tl_calendar_events':
+                $statement = $this->connection->prepare('SELECT title FROM tl_calendar_events WHERE id=?');
+
+                break;
+
+            default:
+                // HOOK: support custom modules
+                if (!isset($GLOBALS['TL_HOOKS']['listComments']) || !is_array($GLOBALS['TL_HOOKS']['listComments'])) {
+                    return $title;
+                }
+
+                $statement = $this->connection
+                    ->prepare('SELECT * FROM tl_comments WHERE source=? AND parent=? LIMIT  0,1');
+
+                $statement->execute([$source, $sourceId]);
+                if ($statement->rowCount() === 0) {
+                    return $title;
+                }
+
+                $row = $statement->fetch(PDO::FETCH_ASSOC);
+                foreach ($GLOBALS['TL_HOOKS']['listComments'] as $callback) {
+                    $callback[0] = System::importStatic($callback[0]);
+
+                    if ($tmp = $callback[0]->{$callback[1]}($row)) {
+                        $title .= $tmp;
+                        break;
+                    }
+                }
+
+                return $title;
+        }
+
+        $statement->execute([$sourceId]);
+        if ($statement->rowCount() === 1) {
+            $title .= ' - ' . $statement->fetchColumn(0);
+        }
+
+        return $title;
     }
 }
